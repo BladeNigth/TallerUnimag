7606a302567fbb8cd70890bd029d7f2b0839fe44
Andres Felipe Brieva Pinedo <53844420+BladeNigth@users.noreply.github.com>
2020-09-06 16:11:17 -0500

    validado los datos vacios en la segunda actividad perfectamente funcional

--- app/src/main/java/co/edu/unimagdalena/apmoviles/tallerunimag/SecondActivity.java
+++ app/src/main/java/co/edu/unimagdalena/apmoviles/tallerunimag/SecondActivity.java
@@ -3,6 +3,7 @@ package co.edu.unimagdalena.apmoviles.tallerunimag;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,66 +37,71 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View view) {
-        int num1 = Integer.parseInt( n1.getText().toString());
-        int num2 = Integer.parseInt( n2.getText().toString());
-        float num3 = Integer.parseInt(n1.getText().toString());
-        float num4 = Integer.parseInt(n2.getText().toString());
-        switch (view.getId()){
-            case R.id.btnsumar:
 
-                Toast.makeText(this, "Suma = " + (num1 + num2),Toast.LENGTH_LONG).show();
-                break;
+        if(TextUtils.isEmpty(n1.getText().toString()) ||
+                TextUtils.isEmpty(n2.getText().toString())){
+            Toast.makeText(getApplicationContext(), "Datos invalidos", Toast.LENGTH_LONG).show();
+        }else {
 
-            case R.id.btnrestar:
-                Toast.makeText(this, "Resta = " + (num1 - num2),Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btnmultiplicar:
-                Toast.makeText(this,"Multiplicacion = " + (num1 * num2),Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btnDividir:
-                if(num4 == 0){
-                    Toast.makeText(getApplicationContext(), "Datos invalidos", Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(this,"Divicion = " + (num3 / num4),Toast.LENGTH_LONG).show();
-                }
-                break;
-            case R.id.btnmayor:
-                if(num1 > num2){
-                    Toast.makeText(this,"el Mayor es = "+ num1,Toast.LENGTH_LONG).show();
-                }else if(num2 > num1){
-                    Toast.makeText(this,"el Mayor es = "+ num2,Toast.LENGTH_LONG).show();
-                }else if(num1 == num2 ){
-                    Toast.makeText(this,"son Iguales ",Toast.LENGTH_LONG).show();
-                }
-                break;
-            case R.id.btnmcm:
-                int mcm = 1;
-                int i=2;
-                while(i <= num1 || i <= num2)
-                {
-                    if(num1%i==0 || num2%i==0)
-                    {
-                        mcm=mcm*i;
-                        if(num1%i==0) num1=num1/i;
-                        if(num2%i==0) num2=num2/i;
+
+            int num1 = Integer.parseInt(n1.getText().toString());
+            int num2 = Integer.parseInt(n2.getText().toString());
+            float num3 = Integer.parseInt(n1.getText().toString());
+            float num4 = Integer.parseInt(n2.getText().toString());
+            switch (view.getId()) {
+                case R.id.btnsumar:
+
+                    Toast.makeText(this, "Suma = " + (num1 + num2), Toast.LENGTH_LONG).show();
+                    break;
+
+                case R.id.btnrestar:
+                    Toast.makeText(this, "Resta = " + (num1 - num2), Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.btnmultiplicar:
+                    Toast.makeText(this, "Multiplicacion = " + (num1 * num2), Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.btnDividir:
+                    if (num4 == 0) {
+                        Toast.makeText(getApplicationContext(), "Datos invalidos", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(this, "Divicion = " + (num3 / num4), Toast.LENGTH_LONG).show();
                     }
-                    else
-                        i=i+1;
-                }
-                Toast.makeText(this,"mcm es:  " + mcm,Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btnmcd:
-                int mcd = 0;
-                int a = Math.max(num1,num2);
-                int b = Math.min(num1,num2);
-                do{
-                    mcd = b;
-                    b = a%b;
-                    a = mcd;
-                }while(b!=0);
-                Toast.makeText(this,"MCD es: "+mcd,Toast.LENGTH_LONG).show();
-                break;
+                    break;
+                case R.id.btnmayor:
+                    if (num1 > num2) {
+                        Toast.makeText(this, "el Mayor es = " + num1, Toast.LENGTH_LONG).show();
+                    } else if (num2 > num1) {
+                        Toast.makeText(this, "el Mayor es = " + num2, Toast.LENGTH_LONG).show();
+                    } else if (num1 == num2) {
+                        Toast.makeText(this, "son Iguales ", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                case R.id.btnmcm:
+                    int mcm = 1;
+                    int i = 2;
+                    while (i <= num1 || i <= num2) {
+                        if (num1 % i == 0 || num2 % i == 0) {
+                            mcm = mcm * i;
+                            if (num1 % i == 0) num1 = num1 / i;
+                            if (num2 % i == 0) num2 = num2 / i;
+                        } else
+                            i = i + 1;
+                    }
+                    Toast.makeText(this, "mcm es:  " + mcm, Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.btnmcd:
+                    int mcd = 0;
+                    int a = Math.max(num1, num2);
+                    int b = Math.min(num1, num2);
+                    do {
+                        mcd = b;
+                        b = a % b;
+                        a = mcd;
+                    } while (b != 0);
+                    Toast.makeText(this, "MCD es: " + mcd, Toast.LENGTH_LONG).show();
+                    break;
 
+            }
         }
     }
 }
